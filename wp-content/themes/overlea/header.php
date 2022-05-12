33a165f735e78bd8819b059f61294c2cd8156a3b
Joe Allen <hi.joeallen@gmail.com>
2022-05-12 15:12:56 -0400

    update w/ GTM

--- wp-content/themes/overlea/header.php
+++ wp-content/themes/overlea/header.php
@@ -19,7 +19,13 @@ use Timber\Post;
 <html <?php language_attributes(); ?>>
 	<head>
 		<?php if ( 'production' === $_ENV['location'] ) : ?>
-			<!-- Paste GA/GTag code here -->
+			<!-- Google Tag Manager -->
+			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-P275H5G');</script>
+			<!-- End Google Tag Manager -->
 		<?php endif; ?>
 
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -43,7 +49,10 @@ use Timber\Post;
 		<?php wp_body_open(); ?>
 
 		<?php if ( 'production' === $_ENV['location'] ) : ?>
-			<!-- Paste GA/GTag code here -->
+			<!-- Google Tag Manager (noscript) -->
+			<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P275H5G"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+			<!-- End Google Tag Manager (noscript) -->
 		<?php endif; ?>
 
 		<article class="v-unsupported-browser">
